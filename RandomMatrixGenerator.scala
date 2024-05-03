6ee9bb41d4063669a73606bcff69ad3b382ff09f
gyt0308 <118203672+gyt0308@users.noreply.github.com>
2024-05-03 17:08:35 +0300

    Delete RandomMatrixGenerator.scala

--- RandomMatrixGenerator.scala
+++
@@ -1,8 +0,0 @@
-import breeze.linalg.DenseMatrix
-import breeze.stats.distributions.Gaussian
-
-object RandomMatrixGenerator {
-  def generate(rows: Int, cols: Int): DenseMatrix[Double] = {
-    DenseMatrix.rand(rows, cols, Gaussian(0, 1))
-  }
-}
