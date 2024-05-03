7846a0dfeac899bdde8a2389ff6b4ecf7c309cd4
gyt0308 <118203672+gyt0308@users.noreply.github.com>
2024-05-03 17:15:36 +0300

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,9 @@
+LinearRegressionProject/
+│
+├── build.sbt                # Конфигурация сборки
+├── src/
+│   ├── main/scala/
+│   │   ├── RandomMatrixGenerator.scala  # Генерация случайных матриц
+│   │   ├── LinearRegressionApp.scala    # Приложения для моделирования линейной регрессии
+│   ├── test/scala/
+│       ├── LinearRegressionTest.scala   # тест линейной регрессии
