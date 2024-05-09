5cea36d2b90edd6b13050666f10081f48563975d
gyt0308 <118203672+gyt0308@users.noreply.github.com>
2024-05-09 19:14:05 +0300

    Delete bash.sh

--- bash.sh
+++
@@ -1,9 +0,0 @@
-LinearRegressionProject/
-│
-├── build.sbt                # Конфигурация сборки
-├── src/
-│   ├── main/scala/
-│   │   ├── RandomMatrixGenerator.scala  # Генерация случайных матриц
-│   │   ├── LinearRegressionApp.scala    # Приложения для моделирования линейной регрессии
-│   ├── test/scala/
-│       ├── LinearRegressionTest.scala   # тест линейной регрессии
