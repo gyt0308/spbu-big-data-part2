26010b1a17b5810dfe1e864e0b3641a71c36194c
gyt0308 <118203672+gyt0308@users.noreply.github.com>
2024-05-09 19:25:07 +0300

    Delete src directory

--- src/main/scala/org/apache/spark/ml/ScalerParams.scala
+++
@@ -1,9 +0,0 @@
-package org.apache.spark.ml.lr
-
-import org.apache.spark.ml.param.shared.{HasInputCol, HasOutputCol}
-
-
-trait ScalerParams extends HasInputCol with HasOutputCol {
-  def setInputCol(value: String) : Unit = set(inputCol, value)
-  def setOutputCol(value: String): Unit = set(outputCol, value)
-}
--- src/test/LinearRegressionTest.scala
+++
@@ -1,9 +0,0 @@
-import org.scalatest.FunSuite
-
-class LinearRegressionTest extends FunSuite {
-  test("Random matrix generation") {
-    val matrix = RandomMatrixGenerator.generate(100, 3)
-    assert(matrix.rows == 100 && matrix.cols == 3)
-  }
-
-}
